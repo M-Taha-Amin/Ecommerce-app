2b63caef404733eb0cb5474a0bf9814c978fdd88
M-Taha-Amin <mtahaamin.official@gmail.com>
2026-06-19 18:31:01 +0500

    define error and response class

---
+++ backend/src/utils/apiResponse.js
@@ -0,0 +1,21 @@
+class ApiResponse {
+  static generic = (res, statusCode, message, success, payload) => {
+    const response = { success, message };
+    if (payload !== undefined) response.payload = payload;
+    return res.status(statusCode).json(response);
+  };
+
+  static success = (res, message, payload) => {
+    return this.generic(res, 200, message, true, payload);
+  };
+
+  static created = (res, message, payload) => {
+    return this.generic(res, 201, message, true, payload);
+  };
+
+  static noContent = (res, message) => {
+    return this.generic(res, 204, message, true);
+  };
+}
+
+module.exports = ApiResponse;
