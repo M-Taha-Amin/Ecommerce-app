cacb1b7aa4e5ad4d52da44e06412572644e85dbb
M-Taha-Amin <mtahaamin.official@gmail.com>
2026-06-25 11:41:11 +0500

    refactor: make ID validation more robust by looping over all IDs in query params

--- backend/src/middlewares/validateMongodbID.js
+++ backend/src/middlewares/validateMongodbID.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const ApiError = require('../utils/apiError');
 
 exports.validateID = function (req, res, next) {
-  const valid = mongoose.Types.ObjectId.isValid(req.params.id);
-  if (valid) next();
-  else throw new ApiError('Invalid Resource ID', 400);
+  for (const key of Object.keys(req.params)) {
+    const valid = mongoose.Types.ObjectId.isValid(req.params[key]);
+    if (!valid) throw new ApiError('Invalid Resource ID', 400);
+  }
+  next();
 };
